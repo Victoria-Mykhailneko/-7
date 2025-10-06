4d7137525d6ea49eec0ee043963c300f062c70a0
Victoria-Mykhailneko <vickysya2018@gmail.com>
2025-10-06 13:44:10 +0300

    Add project files.

---
+++ ЛР7/ЛР7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int n = 4;
+    int a[n][n];
+    int sum = 0, count = 0;
+
+    cout << "Введіть елементи матриці 4x4:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+            if (a[i][j] % 5 == 0) {
+                sum += a[i][j];
+                count++;
+            }
+        }
+    }
+
+    if (count > 0) {
+        double average = (double)sum / count;
+        cout << "Середнє арифметичне елементів кратних 5: " << average << endl;
+    }
+    else {
+        cout << "Немає елементів, кратних 5." << endl;
+    }
+
+    return 0;
+}
